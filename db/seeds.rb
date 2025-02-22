e2903a0669361041d7af584ec880fead4cb6f056
Patrick Amfo <patrick.amfo@icloud.com>
2025-02-22 17:00:48 +0000

    completed the booking model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-puts "Cleaning up database..."
-Yacht.destroy_all
-puts "Database cleaned"
-
-red_yatch = Yacht.new(name: "red", model: "admiral", description: "Red", size: 12, price: 200)
-red_yatch.save
-
-blue_yatch = Yacht.new(name: "blue", model: "admiral", description: "Blue", size: 12, price: 300)
-blue_yatch.save
-
-yellow_yatch = Yacht.new(name: "yellow", model: "admiral", description: "Yellow", size: 12, price: 500)
-yellow_yatch.save
-
-black_yatch = Yacht.new(name: "black", model: "admiral", description: "Black", size: 12, price: 700)
-black_yatch.save
