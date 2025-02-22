ad6ec59a84766d294bcef680b58f806841918945
Alexander Tan <alexandertan527@gmail.com>
2025-02-22 21:32:10 +0000

    adding user_id column to Yacht table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_22_164241) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_22_213008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,8 +45,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_22_164241) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_yachts_on_user_id"
   end
 
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "yachts"
+  add_foreign_key "yachts", "users"
 end
