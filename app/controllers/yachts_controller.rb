2d7cb9274d866631610fbcbfdf586be0ab10e551
Patrick Amfo <patrick.amfo@icloud.com>
2025-02-27 19:07:16 +0000

    Add dotenv-rails

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -1,5 +1,10 @@
 class YachtsController < ApplicationController
   def show
     @yacht = Yacht.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @yacht = Yacht.new
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+red_yacht = Yacht.new(name: "red", model: "007", size: 12, price: 250, description: "The best red yacht")
+new_user = User.new(email: "email@email.com", password: "password")
+new_user.save
+red_yacht.user = new_user
+red_yacht.save
