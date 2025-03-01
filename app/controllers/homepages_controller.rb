a8036c5bdd3cac5553b06141e328491df7919973
Henry Thomas <henrykun95@gmail.com>
2025-03-01 16:30:05 +0000

    edited homepage

--- app/controllers/homepages_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomePagesController < ApplicationController
-  def home
-    @featured_yachts = Yacht.limit(3)
-    # OR
-    # @featured_yachts = Yacht.where(available: true).limit(6)
-  end
-end
