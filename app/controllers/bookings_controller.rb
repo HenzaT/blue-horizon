57b55fc56ab19d5f13796ee616ed5ee7c1f0505d
Alexander Tan <alexandertan527@gmail.com>
2025-03-04 20:52:01 +0000

    booking price calculation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,20 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.yacht = @yacht
     @booking.user =  current_user
+
+    start_year = params["booking"]["start_date(1i)"]
+    start_month = params["booking"]["start_date(2i)"]
+    start_day = params["booking"]["start_date(3i)"]
+    start_date = Time.new(start_year, start_month, start_day)
+
+    end_year = params["booking"]["end_date(1i)"]
+    end_month = params["booking"]["end_date(2i)"]
+    end_day = params["booking"]["end_date(3i)"]
+    end_date = Time.new(end_year, end_month, end_day)
+
+    booking_days = ((end_date - start_date) + 86400)/86400
+    @booking.price = @yacht.price * booking_days
+
     if @booking.save
       redirect_to yacht_path(@yacht), notice: "Booking was successfully created!!."
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_27_192419) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_04_191956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_27_192419) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["yacht_id"], name: "index_bookings_on_yacht_id"
   end
